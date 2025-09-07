828473a9d0f215515f33717749fb0322dae12215
miloantonio73-cloud <miloantonio73@gmail.com>
2025-09-07 14:33:23 +0200

    Add files via upload

--- cloud.js
+++ cloud.js
@@ -1,3 +1,13 @@
 
 console.log("PetPal Cloud.js caricato correttamente!");
 alert("Benvenuto su PetPal!");
+
+function aggiungiNota() {
+    let lista = document.getElementById("lista-diario");
+    let nuovaNota = prompt("Inserisci una nuova nota per il diario:");
+    if (nuovaNota) {
+        let li = document.createElement("li");
+        li.textContent = nuovaNota;
+        lista.appendChild(li);
+    }
+}
